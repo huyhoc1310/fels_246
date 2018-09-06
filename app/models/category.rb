b4ae68a9f9ca324d3c1e1353d1389cee2760c6bf
ChuThom610 <chu.thi.thom@framgia.com>
2018-09-06 10:14:12 +0700

    [Admin] Create, show category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   has_many :words
+
+  validates :name, presence: true,
+    length: {maximum: Settings.model.categories.name.maximum}
+  validates :description, presence: true,
+    length: {maximum: Settings.model.categories.description.maximum}
 end

4f20b19ed10c32dffcbb2ba03c21ebe6901c6713
Nguyen Duc Huy Hoc <huyhoc1310@gmail.com>
2018-09-11 22:20:16 +0700

    create words

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :categories, except: [:show]
+    resources :words 
   end
   resources :categories, only: [:index, :show]
 end
